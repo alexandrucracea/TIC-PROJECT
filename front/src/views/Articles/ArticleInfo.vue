f598c37ed7245de3ba73141c2577d9f70ee9e66b
alexandrucracea <alexandru.cracea29@gmail.com>
2023-01-22 20:09:01 +0200

    Commit V1.1

---
+++ front/src/views/Articles/ArticleInfo.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <h1>Article General Information</h1>
+    <div>
+      <h2>{{ name }}</h2>
+      <p>{{ period }}</p>
+      <p>{{ date }}</p>
+      <p>{{ condition }}</p>
+      <ul>
+        <li v-for="color in colors" :key="color">{{ color }}</li>
+      </ul>
+      <p>{{ description }}</p>
+      <p>{{ price }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ArticleGeneralInfo",
+  props: {
+    id: {
+      type: String,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      article: null,
+    };
+  },
+  created() {
+    this.getArticle();
+  },
+  computed: {
+    name() {
+      return this.article?.name || "";
+    },
+    period() {
+      return this.article?.period || "";
+    },
+    date() {
+      return this.article?.date || "";
+    },
+    condition() {
+      return this.article?.condition || "";
+    },
+    description() {
+      return this.article?.description || "";
+    },
+    price() {
+      return this.article?.price || "";
+    },
+    colors() {
+      return this.article?.colors || [];
+    },
+  },
+  methods: {
+    async getArticle() {
+      this.article = await this.$store.dispatch("loadArticle", this.id);
+    },
+  },
+};
+</script>
